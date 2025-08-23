cf9db167f7ffe5ed230a1e3e63c6ef4707e4bb92
Gideon Adeti <gideonadeti0@gmail.com>
2025-08-23 11:49:16 +0000

    feat(bookings): Implement gRPC methods for booking operations
    Added create, findAll, and findOne methods to the BookingsService
    and BookingsController to handle booking-related operations via
    gRPC. This enhances the service's functionality and allows for
    better integration with the microservices architecture.

--- apps/bookings/src/bookings.controller.ts
+++ apps/bookings/src/bookings.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+
 import { BookingsService } from './bookings.service';
+import {
+  BOOKINGS_SERVICE_NAME,
+  CreateRequest,
+  FindAllRequest,
+  FindOneRequest,
+} from '@app/protos/generated/bookings';
 
 @Controller()
 export class BookingsController {
   constructor(private readonly bookingsService: BookingsService) {}
 
-  @Get()
-  getHello(): string {
-    return this.bookingsService.getHello();
+  @GrpcMethod(BOOKINGS_SERVICE_NAME)
+  create(data: CreateRequest) {
+    return this.bookingsService.create(data);
+  }
+
+  @GrpcMethod(BOOKINGS_SERVICE_NAME)
+  findAll(data: FindAllRequest) {
+    return this.bookingsService.findAll(data);
+  }
+
+  @GrpcMethod(BOOKINGS_SERVICE_NAME)
+  findOne(data: FindOneRequest) {
+    return this.bookingsService.findOne(data.id);
   }
 }
--- apps/bookings/src/bookings.service.ts
+++ apps/bookings/src/bookings.service.ts
@@ -1,8 +1,121 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+
+import { PrismaService } from './prisma/prisma.service';
+import { ServiceType as PrismaServiceType } from '../generated/prisma';
+import {
+  CreateRequest,
+  FindAllRequest,
+  ServiceType,
+} from '@app/protos/generated/bookings';
 
 @Injectable()
 export class BookingsService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prismaService: PrismaService) {}
+
+  private logger = new Logger(BookingsService.name);
+
+  private handleError(error: any, action: string) {
+    this.logger.error(`Failed to ${action}`, (error as Error).stack);
+
+    throw new RpcException(JSON.stringify(error));
+  }
+
+  async create(createRequest: CreateRequest) {
+    try {
+      return await this.prismaService.booking.create({
+        data: {
+          ...createRequest,
+          serviceType: ServiceType[
+            createRequest.serviceType
+          ] as PrismaServiceType,
+          startsAt: createRequest.startsAt as Date,
+        },
+      });
+    } catch (error) {
+      this.handleError(error, 'create booking');
+    }
+  }
+
+  async findAll(findAllRequest: FindAllRequest) {
+    const { userId, isPast, sortBy, order, limit, page } = findAllRequest;
+
+    try {
+      if (!page && !limit) {
+        const bookings = await this.prismaService.booking.findMany({
+          where: {
+            userId,
+            ...(isPast !== undefined && {
+              startsAt: {
+                ...(isPast ? { lt: new Date() } : { gte: new Date() }),
+              },
+            }),
+          },
+          orderBy: { [sortBy || 'createdAt']: order || 'desc' },
+        });
+
+        return {
+          bookings,
+          meta: {},
+        };
+      }
+
+      const total = await this.prismaService.booking.count({
+        where: {
+          userId,
+          ...(isPast !== undefined && {
+            startsAt: {
+              ...(isPast ? { lt: new Date() } : { gte: new Date() }),
+            },
+          }),
+        },
+      });
+
+      const numberPage = page || 1;
+      const numberLimit = limit || 10;
+      const lastPage = Math.ceil(total / numberLimit) || 1;
+      const bookings = await this.prismaService.booking.findMany({
+        where: {
+          userId,
+          ...(isPast !== undefined && {
+            startsAt: {
+              ...(isPast ? { lt: new Date() } : { gte: new Date() }),
+            },
+          }),
+        },
+        orderBy: { [sortBy || 'createdAt']: order || 'desc' },
+        skip: (numberPage - 1) * numberLimit,
+        take: numberLimit,
+      });
+
+      return {
+        bookings,
+        meta: {
+          total,
+          currentPage: numberPage,
+          lastPage,
+          hasNextPage: numberPage < lastPage,
+          hasPreviousPage: numberPage > 1,
+        },
+      };
+    } catch (error) {
+      this.handleError(error, 'fetch bookings');
+    }
+  }
+
+  async findOne(id: string) {
+    try {
+      const booking = await this.prismaService.booking.findUnique({
+        where: { id },
+      });
+
+      if (!booking) {
+        throw new NotFoundException(`Booking with id ${id} not found`);
+      }
+
+      return booking;
+    } catch (error) {
+      this.handleError(error, `fetch booking with id ${id}`);
+    }
   }
 }
