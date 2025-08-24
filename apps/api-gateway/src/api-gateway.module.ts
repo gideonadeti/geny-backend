b4e2ca8d30d44edf83a1a707a5157a87419a6fb5
Gideon Adeti <gideonadeti0@gmail.com>
2025-08-24 07:10:29 +0000

    feat(api-gateway): Add BookingsModule to ApiGatewayModule

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
+import { BookingsModule } from './bookings/bookings.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
       envFilePath: 'apps/api-gateway/.env',
     }),
+    BookingsModule,
   ],
   controllers: [],
   providers: [],
