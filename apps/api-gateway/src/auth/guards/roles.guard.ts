65a5de61ad53be601783acf3fa92f8e724b31f00
Gideon Adeti <gideonadeti0@gmail.com>
2025-08-25 15:47:57 +0000

    chore: move decorators and guards to auth folder

--- apps/api-gateway/src/auth/guards/roles.guard.ts
+++ apps/api-gateway/src/auth/guards/roles.guard.ts
@@ -4,12 +4,22 @@ import { Request } from 'express';
 
 import { UserRole, User } from '@app/protos/generated/auth';
 import { ROLES_KEY } from '../decorators/roles.decorator';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
       ROLES_KEY,
       [context.getHandler(), context.getClass()],
